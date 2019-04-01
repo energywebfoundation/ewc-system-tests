2969ab01ec3de10f169f9f6cb380195387d3c5c0
danzipie <danzipie@gmail.com>
2019-04-01 17:36:31 +0200

    first two tests included

--- test_sanity/test.js
+++ test_sanity/test.js
@@ -10,43 +10,44 @@ var web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8450'))
 const VALUES = "../ewf-genesis-generator/sample_chainspc/hardcoded_values.json"
 const CONTRACT = "ValidatorSetRelayed"
 var values = {};
-var contract;
 
-async function callAsync(contract) {
-  contract.methods.getValidators().call((error, result) => {
-  });
-  
+// functions
+async function getOwner(contract) {
+  return await contract.methods.owner().call();
 }
 
-function initEverything(done) {
-  web3.eth.net.isListening()
-  .then(function() {
-    console.log("Retrieving hard-coded addresses")
-    fs.readFile(VALUES, (err, jso) => {  
-      if (err) throw err;
-      values = JSON.parse(jso);
+// tests
+describe(' Contracts', function() {
 
-      console.log("Retrieving contract ABI")
-      fs.readFile('../genome-system-contracts/build/contracts/' + CONTRACT + '.json', (err, me) => {  
-          if (err) throw err;
-          let RelayContractABI = JSON.parse(me); 
-          
-          contract = web3.eth.Contract(RelayContractABI.abi, values.address_book["RELAYED_CONTRACT"])
-          console.log(contract)
-          done()
+  var relayed;
+  let RelayContractABI;
 
-      });
+  async function initEverything(done) {
+    let listening = await web3.eth.net.isListening();
+    let jso = fs.readFileSync(VALUES, 'utf-8');
+    values = JSON.parse(jso);
+    let me = fs.readFileSync('../genome-system-contracts/build/contracts/' + CONTRACT + '.json', 'utf-8');
+    RelayContractABI = JSON.parse(me);
+  }
 
-    })     
+  before(async function (){
+    await initEverything();
+    relayed = new web3.eth.Contract(RelayContractABI.abi, values.address_book["VALIDATOR_RELAYED"]);
+  });
+
+  describe('Validator Relayed', function() {
+
+    it('should be owned by NetOps', async function () {
+      (await relayed.methods.owner.call()).should.be.equal(values.address_book["VALIDATOR_NETOPS"]);
+    });
+
+    it('should set current list of validators correctly', async function () {
+      let accounts = values.address_book["INITAL_VALIDATORS"];
+      const validatorsList = [accounts[0], accounts[1], accounts[2]];
+      let validators = await relayed.methods.getValidators.call();
+      validators.should.be.deep.equal(validatorsList);
+    });
+
   })
-  .catch(e => console.log('Wow. Something went wrong.'));
-}
 
-describe('Validator Contracts', function() {
-
-  before(initEverything)
-
-  it('should resolve', (contract) => {
-    return callAsync(contract);
-  });
 });

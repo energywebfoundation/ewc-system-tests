5eed3ba0b6787f87bfc0e36ad89199edc9653204
danzipie <danzipie@gmail.com>
2019-04-02 13:13:28 +0200

    add tests

--- test_sanity/test.js
+++ test_sanity/test.js
@@ -20,7 +20,10 @@ async function getOwner(contract) {
 describe(' Contracts', function() {
 
   var relayed;
+  var netOpsMultiSig;
+  var communityMultiSig;
   let RelayContractABI;
+  let MultiSigABI;
 
   async function initEverything(done) {
     let listening = await web3.eth.net.isListening();
@@ -28,11 +31,15 @@ describe(' Contracts', function() {
     values = JSON.parse(jso);
     let me = fs.readFileSync('../genome-system-contracts/build/contracts/' + CONTRACT + '.json', 'utf-8');
     RelayContractABI = JSON.parse(me);
+    me = fs.readFileSync('../MultiSigWallet/build/contracts/MultiSigWallet.json', 'utf-8');
+    MultiSigABI = JSON.parse(me);
   }
 
   before(async function (){
     await initEverything();
     relayed = new web3.eth.Contract(RelayContractABI.abi, values.address_book["VALIDATOR_RELAYED"]);
+    netOpsMultiSig = new web3.eth.Contract(MultiSigABI.abi, values.address_book["VALIDATOR_NETOPS"]);
+    communityMultiSig = new web3.eth.Contract(MultiSigABI.abi, values.address_book["COMMUNITY_FUND"]);
   });
 
   describe('Validator Relayed', function() {
@@ -44,10 +51,31 @@ describe(' Contracts', function() {
     it('should set current list of validators correctly', async function () {
       let accounts = values.address_book["INITAL_VALIDATORS"];
       const validatorsList = [accounts[0], accounts[1], accounts[2]];
-      let validators = await relayed.methods.getValidators.call();
+      var validators = await relayed.methods.getValidators.call();
+      validators = validators.map(v => v.toLowerCase());
       validators.should.be.deep.equal(validatorsList);
     });
+    // @TODO: other tests for validator relayed here
+  });
 
-  })
+  describe('NetOps MultiSigWallet', function() {
+
+    it('should be owned by NetOps', async function () {
+      let addresses = await communityMultiSig.methods.getOwners.call();
+      addresses = addresses.map(v => v.toLowerCase());
+      (await addresses).should.be.deep.equal(values.address_book["COMMUNITY_FUND_MEMBERS"]);
+    });
+    
+  });
+
+  describe('Community Fund MultiSigWallet', function() {
+
+    it('should be owned by Community Fund', async function () {
+      let addresses = await netOpsMultiSig.methods.getOwners.call();
+      addresses = addresses.map(v => v.toLowerCase());
+      (await addresses).should.be.deep.equal(values.address_book["NETOPS_MEMBERS"]);
+    });
+    
+  });
 
 });

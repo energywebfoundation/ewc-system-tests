5855a1daa9a3b9cfddd147798f566a414542e06f
danzipie <danzipie@gmail.com>
2019-04-05 16:07:30 +0200

    rename and fix

--- utils/fund_netops.py
+++ utils/fund_netops.py
@@ -1,3 +1,7 @@
+# funds the NetOps team with tokens
+# can only be called from account that has tokens
+# requires running Parity in the background
+
 from web3.auto import w3
 import os
 import json
@@ -5,14 +9,14 @@ import time
 import sys
 import argparse
 
-VALUE = 1000000000000000000
+VALUE = 1000000000000000000 # amount
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--keydir', help="Directory where you store the key file", required=True)
 args = parser.parse_args()
 
 beneficiaries = []
-with open('../../ewf-genesis-generator/sample_chainspc/hardcoded_values.json') as json_file:  
+with open('./node_modules/ewf-genesis-generator/chainspec_skeletons/hardcoded_values_volta.json') as json_file:  
     hc = json.load(json_file)
     for address in hc['address_book']['NETOPS_MEMBERS']:
         beneficiaries.append(address)
@@ -29,17 +33,18 @@ with open(pkey) as keyfile:
     print('Sending from ' + address)
 
     nonce = w3.eth.getTransactionCount(address)
+    print(beneficiaries)
     for beneficiary in beneficiaries:
         beneficiary = w3.toChecksumAddress(beneficiary)
-        if not beneficiary == address: 
-            transaction = {
-                'to': beneficiary,
-                'value': VALUE,
-                'gas': 46000,
-                'gasPrice': 5,
-                'nonce': nonce,
-                'chainId': 0x12046
-            }
-            print('Sending ' + VALUE + ' to ' + beneficiary)
-            signed_tx = w3.eth.account.signTransaction(transaction, private_key)
-            w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        transaction = {
+            'to': beneficiary,
+            'value': VALUE,
+            'gas': 46000,
+            'gasPrice': 5,
+            'nonce': nonce,
+            'chainId': 0x12046
+        }
+        nonce = nonce + 1
+        print('Sending ' + str(VALUE) + ' to ' + beneficiary)
+        signed_tx = w3.eth.account.signTransaction(transaction, private_key)
+        w3.eth.sendRawTransaction(signed_tx.rawTransaction)

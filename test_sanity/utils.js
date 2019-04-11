1e42860bf4fcb61e785e12c1613a672f4430ffc7
ngyam <ngy.am.zs@gmail.com>
2019-04-11 11:56:17 +0200

    feat(blockreward-unittests): unittests ported
    - added BN and chai as promised to package json
    - utils extended
    - blockreward unittest ports with S curve function
    - test script include all test_sanity files with test.js ending

--- test_sanity/utils.js
+++ test_sanity/utils.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 
-const REVERT_ERROR_MSG = "VM Exception while processing transaction: revert";
+const REVERT_ERROR_MSG = "Transaction has been reverted by the EVM";
 const DEFAULT_ADDRESS = "0x0000000000000000000000000000000000000000";
 const SYSTEM_ADDRESS = "0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE";
 const EMPTY_BYTES32 = "0x0000000000000000000000000000000000000000000000000000000000000000";
-
-const CHAINSPEC_VALUES = "./node_modules/ewf-genesis-generator/sample_chainspec/chainspec_skeletons/hardcoded_values_volta.json";
 
 const ValidatorState = {
     NonValidator: "0",
@@ -13,6 +11,11 @@ const ValidatorState = {
     PendingToBeAdded: "2",
     PendingToBeRemoved: "3"
 }
+
+const testValidators = ["0xdd520827a407b44753723ceb2614b5e0cbb1cd69", "0x18e089e83026fac89c029762a901d90b7acc0d2b", "0xca51975cccdf82bda569585f8ed12b0781a05740"];
+// validator 1-3 + community
+const testPayoutAddresses = ["0x00371459b526eA286E3e898950cE8F713B540f0B","0x00d99132c825Ecfd224832EDe811177ad1512610","0x0019E00282A6C64d0B2a37df7B9c98B298cc605A","0x000E9A1A767eA9B3A092ED49c45Eb7f8e318BD39"];
+
 
 const send = (method, params = []) => {
     return new Promise((resolve, reject) => web3.currentProvider.send({id: 0, jsonrpc: '2.0', method, params }, (e, data) => {
@@ -44,7 +47,7 @@ async function sendMultisigTransaction(web3, multisig, transaction, destination,
     multisig.transactionConfirmationBlocks = 1
 
     const submitter = web3.eth.accounts.wallet.accounts[submitterWalletPosition].address;
-    const confirmer = submitterWalletPosition == 0 ? web3.eth.accounts.wallet.accounts['1'].address : web3.eth.accounts.wallet.accounts[submitterWalletPosition].address;
+    const confirmer = submitterWalletPosition == 0 ? web3.eth.accounts.wallet.accounts['1'].address : web3.eth.accounts.wallet.accounts['0'].address;
     
     const submitGas = await multisig.methods.submitTransaction(destination, web3.utils.toHex(transaction.value), transaction.data).estimateGas({from: submitter});
     const logs = await multisig.methods.submitTransaction(destination, web3.utils.toHex(transaction.value), transaction.data).send({
@@ -61,11 +64,29 @@ async function sendMultisigTransaction(web3, multisig, transaction, destination,
     });
 }
 
-ChainspecValues =
+const ChainspecValues =
     JSON.parse(
         fs.readFileSync(__dirname + "/../node_modules/ewf-genesis-generator/chainspec_skeletons/hardcoded_values_volta.json")
     );
 
+const MultiSigWalletJSON =
+    JSON.parse(
+        fs.readFileSync(__dirname + "/../node_modules/multisig-wallet-gnosis/build/contracts/MultiSigWallet.json")
+    );
+
+const RelayedJSON = JSON.parse(
+        fs.readFileSync(__dirname + "/../node_modules/genome-system-contracts/build/contracts/ValidatorSetRelayed.json")
+)
+;
+const RelayJSON = JSON.parse(
+        fs.readFileSync(__dirname + "/../node_modules/genome-system-contracts/build/contracts/ValidatorSetRelay.json")
+);
+
+const BlockRewardJSON = JSON.parse(
+        fs.readFileSync(__dirname + "/../node_modules/genome-system-contracts/build/contracts/BlockReward.json")
+);
+
+const TestSCurveProvder = require("./blockreward_function");
 
 module.exports = {
     assertThrowsAsync,
@@ -76,5 +97,12 @@ module.exports = {
     sendMultisigTransaction,
     addTestWallets,
     ValidatorState,
-    ChainspecValues
+    ChainspecValues,
+    MultiSigWalletJSON,
+    RelayedJSON,
+    RelayJSON,
+    BlockRewardJSON,
+    testValidators,
+    testPayoutAddresses,
+    TestSCurveProvder
 }

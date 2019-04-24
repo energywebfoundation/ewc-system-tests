ce043a38c24e4f0324fd3e1f68f4386a55be2a49
ngyam <ngy.am.zs@gmail.com>
2019-04-24 17:56:52 +0200

    update(namings): genome -> ewc

--- test_sanity/utils.js
+++ test_sanity/utils.js
@@ -96,15 +96,15 @@ const MultiSigWalletJSON =
     );
 
 const RelayedJSON = JSON.parse(
-        fs.readFileSync(__dirname + "/../node_modules/genome-system-contracts/build/contracts/ValidatorSetRelayed.json")
+        fs.readFileSync(__dirname + "/../node_modules/ewc-system-contracts/build/contracts/ValidatorSetRelayed.json")
 )
 ;
 const RelayJSON = JSON.parse(
-        fs.readFileSync(__dirname + "/../node_modules/genome-system-contracts/build/contracts/ValidatorSetRelay.json")
+        fs.readFileSync(__dirname + "/../node_modules/ewc-system-contracts/build/contracts/ValidatorSetRelay.json")
 );
 
 const BlockRewardJSON = JSON.parse(
-        fs.readFileSync(__dirname + "/../node_modules/genome-system-contracts/build/contracts/BlockReward.json")
+        fs.readFileSync(__dirname + "/../node_modules/ewc-system-contracts/build/contracts/BlockReward.json")
 );
 
 const TestSCurveProvder = require("./blockreward_function");
